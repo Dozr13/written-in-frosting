565c7088aff9af723bb820ddc1fe25f7e0baa825
Wade Pate <wadejp8@gmail.com>
2021-05-18 14:41:43 -0600

    stash changes

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,50 +1,205 @@
-import React, {useState} from 'react';
-// import {css, jsx} from '@emotion/core';
-import styled from 'styled-components';
-import SliderContent from './SliderContent';
+/** @jsx jsx */
+/** @jsxRuntime classic */
+
+import { jsx } from 'theme-ui';
+
+import React, { useState, useEffect, useRef, useCallback } from 'react'
+import { css } from '@emotion/core'
+import SliderContent from './SliderContent'
 import Slide from './Slide'
+import Arrow from './Arrow'
+import Dots from './Dots'
 
+const getWidth = () => window.innerWidth
 
+/**
+ * @function Slider
+ */
+const Slider = props => {
+  const { slides } = props
 
-// ************* \\
-  // * @function Slider;  the Slider parent component * \\
-// ************* \\
-
-const Slider = (props) => {
-  const getWidth = () => window.innerWidth;
+  const firstSlide = slides[0]
+  const secondSlide = slides[1]
+  const lastSlide = slides[slides.length - 1]
 
   const [state, setState] = useState({
-    translate: 0,
+    activeSlide: 0,
+    translate: getWidth(),
     transition: 0.45,
+    _slides: [lastSlide, firstSlide, secondSlide]
   })
 
-  const {translate, transition} = state
+  const { activeSlide, translate, _slides, transition } = state
+
+  const autoPlayRef = useRef()
+  const transitionRef = useRef()
+  const resizeRef = useRef()
+  const sliderRef = useRef()
+
+  useEffect(() => {
+    autoPlayRef.current = nextSlide
+    transitionRef.current = smoothTransition
+    resizeRef.current = handleResize
+  })
+
+  useEffect(() => {
+    const slider = sliderRef.current
+
+    const play = () => {
+      autoPlayRef.current()
+    }
+
+    const smooth = e => {
+      if (e.target.className.includes('SliderContent')) {
+        transitionRef.current()
+      }
+    }
+
+    const resize = () => {
+      resizeRef.current()
+    }
+
+    const transitionEnd = slider.addEventListener('transitionend', smooth)
+    const onResize = window.addEventListener('resize', resize)
+
+    let interval = null
+
+    if (props.autoPlay) {
+      interval = setInterval(play, props.autoPlay * 1000)
+    }
+
+    return () => {
+      slider.removeEventListener('transitionend', transitionEnd)
+      window.removeEventListener('resize', onResize)
+
+      if (props.autoPlay) {
+        clearInterval(interval)
+      }
+    }
+  }, [])
+
+  useEffect(() => {
+    if (transition === 0) setState({ ...state, transition: 0.45 })
+  }, [transition])
+
+  const handleResize = () => {
+    setState({ ...state, translate: getWidth(), transition: 0 })
+  }
+
+  const smoothTransition = () => {
+    let _slides = []
+
+    // We're at the last slide.
+    if (activeSlide === slides.length - 1)
+      _slides = [slides[slides.length - 2], lastSlide, firstSlide]
+    // We're back at the first slide. Just reset to how it was on initial render
+    else if (activeSlide === 0) _slides = [lastSlide, firstSlide, secondSlide]
+    // Create an array of the previous last slide, and the next two slides that follow it.
+    else _slides = slides.slice(activeSlide - 1, activeSlide + 2)
+
+    setState({
+      ...state,
+      _slides,
+      transition: 0,
+      translate: getWidth()
+    })
+  }
+
+  const nextSlide = () =>
+    setState({
+      ...state,
+      translate: translate + getWidth(),
+      activeSlide: activeSlide === slides.length - 1 ? 0 : activeSlide + 1
+    })
+
+  const prevSlide = () =>
+    setState({
+      ...state,
+      translate: 0,
+      activeSlide: activeSlide === 0 ? slides.length - 1 : activeSlide - 1
+    })
 
   return (
-    <SlicerCSS>
+    <div css={SliderCSS} ref={sliderRef}>
       <SliderContent
         translate={translate}
         transition={transition}
-        width={getWidth() * props.slides.length}
+        width={getWidth() * _slides.length}
       >
-        {
-          props.slides.map((s, i) => {
-            return(
-              <Slide key={s + i} content={s} />
-            );
-          })
-        }
+        {_slides.map((_slide, i) => (
+          <Slide width={getWidth()} key={_slide + i} content={_slide} />
+        ))}
       </SliderContent>
-    </SlicerCSS>
+
+      <Arrow direction="left" handleClick={prevSlide} />
+      <Arrow direction="right" handleClick={nextSlide} />
+
+      <Dots slides={slides} activeSlide={activeSlide} />
+    </div>
   )
 }
 
-const SlicerCSS = styled.div`
+const SliderCSS = css`
   position: relative;
-  height: 50vh;
-  width: 70vw;
+  height: 100vh;
+  width: 100vw;
   margin: 0 auto;
   overflow: hidden;
-`;
+  white-space: nowrap;
+`
 
-export default Slider;
+export default Slider
+
+
+
+
+// import React, {useState} from 'react';
+// // import {css, jsx} from '@emotion/core';
+// import styled from 'styled-components';
+// import SliderContent from './SliderContent';
+// import Slide from './Slide'
+
+
+
+// // ************* \\
+//   // * @function Slider;  the Slider parent component * \\
+// // ************* \\
+
+// const Slider = (props) => {
+//   const getWidth = () => window.innerWidth;
+
+//   const [state, setState] = useState({
+//     translate: 0,
+//     transition: 0.45,
+//   })
+
+//   const {translate, transition} = state
+
+//   return (
+//     <SlicerCSS>
+//       <SliderContent
+//         translate={translate}
+//         transition={transition}
+//         width={getWidth() * props.slides.length}
+//       >
+//         {
+//           props.slides.map((s, i) => {
+//             return(
+//               <Slide key={s + i} content={s} />
+//             );
+//           })
+//         }
+//       </SliderContent>
+//     </SlicerCSS>
+//   )
+// }
+
+// const SlicerCSS = styled.div`
+//   position: relative;
+//   height: 50vh;
+//   width: 70vw;
+//   margin: 0 auto;
+//   overflow: hidden;
+// `;
+
+// export default Slider;
